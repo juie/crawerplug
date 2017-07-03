9b32435a19e228aed842f39af94bc55c60dcc399
recike <recike@163.com>
2017-07-03 09:27:28 +0800

    修改了第二次通信

--- background.js
+++ background.js
@@ -1,10 +1,20 @@
+function sleep(d) {
+	if (!d) {
+		d = Math.random() * 1000 + 1000;
+		// d = Math.random() * 1500;
+	}
+	for (var t = Date.now(); Date.now() - t <= d;);
+}
+
 //发送消息函数
 function sendMsg(tabId, type, rule, name) {
+	sleep();
+	alert("开始通信");
 	chrome.tabs.sendMessage(tabId, {
 		type: type,
 		rule: rule,
 		name: name
-	});
+	},function(response){alert(response)});
 }
 
 //回调函数,用于省略
@@ -21,12 +31,16 @@ function start(){
 // chrome.tabs.create({"url" :"http://www.gsxt.gov.cn/index.html"});
 start();
 
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo) {
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
 		var link = changeInfo.url;
+		// alert(link);
 		if (link=="http://www.gsxt.gov.cn/corp-query-search-1.html"){
-			chrome.tabs.executeScript(tabId,{file:"match.js"});
+			// chrome.tabs.executeScript(tabId,{file:"match.js"});
+			// sendMsg(tabId,"match","","中国工商银行股份有限公司");
+			// alert(tabId);
+			sendMsg(tabId,"match","","中国工商银行股份有限公司");
 		}else if (link=="http://www.gsxt.gov.cn/index.html"){
-			alert("start");
-			sendMsg(tabId,"content","","重庆三峡银行");
+			alert(tabId);
+			sendMsg(tabId,"content","","中国工商银行股份有限公司");
 		};
 });
--- content.js
+++ content.js
@@ -8,8 +8,23 @@ function start(name){
 	})
 }
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+function getname(){
+  var h1s = document.getElementsByTagName("h1");
+  for (var i=0; i<h1s.length; i++){
+    alert(h1s[i].innerText);
+  }
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if (request.type=="content"){
 		start(request.name);
-	}
+		sendResponse("ok");
+	}else if(request.type=="match"){
+		alert(request);
+		sendResponse("0000000");
+	};
 });
+
+// chrome.tabs.onUpdated.addListener(function(tabId,changeInfo) {
+// 	alert("update")
+// });
